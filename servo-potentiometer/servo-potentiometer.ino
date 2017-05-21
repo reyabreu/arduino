259ee76dd51cee01720523577b500fa5d426212a
Reynaldo Abreu <Reynaldo.Abreu@paysafe.com>
2017-05-21 09:57:12 +0100

    implenmented Exponential Moving Average low pass filter on 22K Pot analog input

--- servo-potentiometer/servo-potentiometer.ino
+++ servo-potentiometer/servo-potentiometer.ino
@@ -1,5 +1,5 @@
 /*
-  pause and upause servo sweep with pushbutton
+  pause and upause micro9g servo sweep with pushbutton. In paused mode set angle of servo by using 22K pot
 */
 #include <Servo.h>
 #include <SoftwareSerial.h>
@@ -10,10 +10,14 @@ const int DIGITAL_LED_PAUSE_PIN = LED_BUILTIN;      // the number of the LED pin
 const int DIGITAL_SERVO1_PIN = 9;
 const int ANALOG_POT_PIN = 0;
 
+const float SAMPLE_WEIGHT = 0.6;  //initialization of EMA alpha
+
 // Variables will change:
 Servo servo1;
 
 int servo1Position = 0;
+int lastServo1Position = 0;
+
 int delta = 45;
 unsigned long lastCommandTime = 0;
 unsigned long servoDelay = 1000;
@@ -21,13 +25,15 @@ unsigned long servoDelay = 1000;
 //button state variables
 int buttonState;
 int lastButtonState = HIGH;
-int lastPotReading = 0;
 
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 
 //loop control variables
 boolean paused = true;
+
+//exponential moving average low pass filtering for pot
+int filteredPotReading = 0;      //filtered pot reading  
 
 void setup() {
   //initialize Serial
@@ -42,6 +48,9 @@ void setup() {
   //attach led - set state
   pinMode(DIGITAL_LED_PAUSE_PIN, OUTPUT);
   digitalWrite(DIGITAL_LED_PAUSE_PIN, LOW);
+
+  //initialise first reading
+  filteredPotReading = analogRead(ANALOG_POT_PIN);
 
   //wait until serial instantiated
   initSerial;
@@ -72,18 +81,17 @@ void loop() {
       if (buttonState == LOW) {
         paused = !paused;
         Serial.print(F("Pause state changed: "));
-        Serial.println(paused);
+        Serial.println(paused == 1 ? F("TRUE") : F("FALSE"));
       }
     }
   }
   digitalWrite(DIGITAL_LED_PAUSE_PIN, paused);  
   lastButtonState = reading;
 
-  //move servo
+ //only bother if enough time has passed since last move   
+  //sweep servo
   if (!paused) {
-    
-    if (millis() - lastCommandTime > servoDelay) {
-      
+   if (millis() - lastCommandTime > servoDelay) {
       Serial.print(F("arm at: "));
       Serial.print(servo1.read());
       Serial.println(F(" degrees"));
@@ -96,18 +104,32 @@ void loop() {
         } else if (servo1Position > 180) {
           servo1Position = 180; 
         };
-      }
-      servo1.write(servo1Position);                  
+      }//end servo boundary check
+      
+      servo1.write(servo1Position);
       lastCommandTime = millis();
-    }
-  } else {
-    int potReading = analogRead(ANALOG_POT_PIN);
-    //reading = map(reading, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-    if (abs(potReading - lastPotReading) > 10) {
-      Serial.print(F("Pot reading: "));
-      Serial.println(potReading);
-      lastPotReading = potReading;
-    }
+    } //end last servo command check
     
-  }
+    } // end not paused option
+    
+   // read pot and move servo
+   else {      
+      int potReading = analogRead(ANALOG_POT_PIN);
+      filteredPotReading = ( SAMPLE_WEIGHT * potReading ) + ( ( 1 - SAMPLE_WEIGHT ) * filteredPotReading );
+
+      servo1Position = map(filteredPotReading, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+      servo1.write(servo1Position);
+      
+      if (millis() - lastCommandTime > servoDelay) {
+        if (abs(lastServo1Position - servo1Position) >= 1){
+          Serial.print(F("Filtered Pot reading: "));
+          Serial.print(filteredPotReading);
+          Serial.print(F(" servo angle mapping: "));
+          Serial.println(servo1Position);
+        }
+        lastCommandTime = millis();
+        lastServo1Position = servo1Position;
+      } //end last servo command check
+    } //end paused option 
+
 }
